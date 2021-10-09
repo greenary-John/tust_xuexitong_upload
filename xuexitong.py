0e82262199af175fcb4b12c2bf6e3fdefeaf8f73
greenary-John <30305342+greenary-John@users.noreply.github.com>
2021-10-09 14:55:07 +0800

    Update xuexitong.py

--- xuexitong.py
+++ xuexitong.py
@@ -4,19 +4,19 @@ from selenium.webdriver.chrome.options import Options
 
 
 chrome_options = Options()
-chrome_options.add_argument('--headless')  # 无头浏览器
-bro = webdriver.Chrome(executable_path=r'谷歌驱动的绝对路径',options=chrome_options)  # 实例化对象
+#chrome_options.add_argument('--headless')  # 无头浏览器
+bro = webdriver.Chrome(executable_path=r'F:/test/chromedriver.exe',options=chrome_options)  # 实例化对象
 bro.get('http://passport2.chaoxing.com/login?fid=&newversion=true&refer=http%3A%2F%2Fi.chaoxing.com')  # 学习通登录url
 username_tag = bro.find_element_by_id('phone')
-username_tag.send_keys('your_mobile_phone')
+username_tag.send_keys('phone')
 password_tag = bro.find_element_by_id('pwd')
-password_tag.send_keys('your_passwd')
+password_tag.send_keys('passwd')
 btn = bro.find_element_by_id('loginBtn')
 btn.click()
 sleep(5)
-bro.get('http://office.chaoxing.com/front/web/apps/forms/fore/apply?uid=131814638&code=vGh1maqe&mappId=5300924&appId=3c9c9642139643ba91477f442d69becc&appKey=l2E6J3d1g0B%2F5bCG&id=14788&enc=06fbc4bd4c56c44afabca4103d961275&state=121890&formAppId=&fidEnc=5901ab09a9ed3537')  # 健康填报url
+bro.get('https://uc.chaoxing.com/mobileSet/homePage?fid=121890&customizecolor=0xFFFFFF&time=20210103085130342&enc=EE9D0B4C0E4393B0C64851D542A6E14F')  # 健康填报url
 sleep(5)
-aniu = bro.find_element_by_xpath('/html/body/div[3]/div/div/div[1]/div[2]/div[1]')
+aniu = bro.find_element_by_xpath('/html/body/div[3]/div/div/div[2]/div[2]/div[1]')
 aniu.click()
 sleep(5)
 morning_tag = bro.find_element_by_xpath('//*[@id="10"]/div[3]/input')
@@ -29,5 +29,5 @@ no_tag = bro.find_element_by_xpath('//*[@id="13"]/div[3]/input')
 no_tag.send_keys('无')
 b = bro.find_element_by_xpath('//*[@id="forms"]/div[2]/div[5]/div/div[2]/p')
 b.click()
-# sleep(5)
+sleep(5)
 bro.quit()
